6d12f2f6200be279dc0e258a5e32d60a04c20378
Sergio Segura Blanco <ssegurab@gmail.com>
2022-03-10 13:29:28 +0100

    Primera subida de archivos al repositorio

---
+++ js/swiper-slider.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+  });
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    }
+});
